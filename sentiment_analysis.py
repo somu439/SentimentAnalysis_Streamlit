54dac46395acf88318bd3048d6f8676e47ff4182
Sreeni <somu439@gmail.com>
2025-08-11 00:02:47 -0400

    Adding necessary libs

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import os
 # import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.metrics import confusion_matrix
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from sklearn.metrics import confusion_matrix
 import plotly.express as px
 from openai import OpenAI
 
